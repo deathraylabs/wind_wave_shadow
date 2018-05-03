ccc137ad1a002eb4800570b242334f95d9ec1f30
pj smith <github@deathraylabs.com>
2018-05-03 16:32:38 -0500

    added: - UTC to local timezone function

--- get_buoy_data_sos.py
+++ get_buoy_data_sos.py
@@ -2,9 +2,26 @@
 
 Details regarding how to access the NDBC data can be found at their SOS website:
 `<https://sdf.ndbc.noaa.gov/sos/>_`
+
+note
+----
+
+- wave directions are reported in the direction they're headed *to*,
+not *from* as they are at other sites.
 """
 
 import requests
+import csv
+from io import StringIO
+from dateutil import parser
+from dateutil import tz
+
+def utc_to_local_time(utc_iso_string):
+    """Convert UTC timestamp to local timestamp (ISO 8601)"""
+    utc_time = parser.parse(utc_iso_string)
+    local_zone = tz.tzlocal()
+
+    return utc_time.astimezone(local_zone)
 
 # this is the SOS server website we'll append our request to
 url_prefix = 'https://sdf.ndbc.noaa.gov/sos/server.php'
@@ -13,8 +30,8 @@ url_prefix = 'https://sdf.ndbc.noaa.gov/sos/server.php'
 payload = {'request': 'GetObservation',
            'service': 'SOS',
            'version': '1.0.0',
-           'offering': 'urn:ioos:station:wmo:41012',
-           'observedproperty': 'air_pressure_at_sea_level',
+           'offering': 'urn:ioos:station:wmo:42019',
+           'observedproperty': 'waves',
            'responseformat': 'text/csv',
            'eventtime': 'latest'}
 
@@ -25,5 +42,16 @@ payload_str =  "&".join("%s=%s" % (k,v) for k,v in payload.items())
 
 # `requests.post()` doesn't work for some reason but get does
 r = requests.get(url_prefix, params=payload_str)
+
+# weather station data file in format csv module can handle
+weather_file = StringIO(r.text)
+
+# convert text data to dictionary
+weather_data = csv.DictReader(weather_file, delimiter=',')
+weather_fieldnames = weather_data.fieldnames
+
+for line in weather_data:
+    for name in weather_fieldnames:
+        print(name + ": " + str(line[name]))
 
 print(r.text)
